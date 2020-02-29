c6861418a63acaac5ac0a15d3772cd122a19287c
Fernando Ortega Gallego <fgallego@plainconcepts.com>
2020-02-29 13:38:18 +0100

    Added a background task to release after X seconds.

--- server/rpi_utils.py
+++ server/rpi_utils.py
@@ -1,4 +1,7 @@
-import io
+import io, atexit
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def is_raspberry_pi(raise_on_errors=False):
@@ -42,23 +45,44 @@ if not is_raspberry_pi():
     import fake_rpi
 
     sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
-    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
+    sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
     sys.modules["smbus"] = fake_rpi.smbus  # Fake smbus (I2C)
 
 import RPi.GPIO as gpio
 
 DEFAULT_OPEN_DOOR_PIN = 23
+DEFAULT_RELEASE_SECONDS = 5
+RELEASE_JOB_ID = "release_job"
 
 
 class PiDoorBellGPIOHelper:
     def __init__(self, open_door_pin=DEFAULT_OPEN_DOOR_PIN):
         self.__open_door_pin = open_door_pin
+        self.__scheduler = BackgroundScheduler()
         gpio.setmode(gpio.BCM)
         gpio.setup(self.__open_door_pin, gpio.OUT)
         gpio.output(self.__open_door_pin, gpio.LOW)
 
+        self.__scheduler.start()
+        # Shut down the scheduler when exiting the app
+        atexit.register(lambda: self.__scheduler.shutdown())
+
     def push(self):
         gpio.output(self.__open_door_pin, gpio.HIGH)
+        self.__delayed_release()
 
     def release(self):
         gpio.output(self.__open_door_pin, gpio.LOW)
+        if self.__scheduler.get_job(job_id=RELEASE_JOB_ID) is not None:
+            self.__scheduler.remove_job(job_id=RELEASE_JOB_ID)
+
+    def __delayed_release(self):
+        print("Added new job to release")
+        self.__scheduler.add_job(
+            id=RELEASE_JOB_ID,
+            max_instances=1,
+            func=self.release,
+            trigger="date",
+            run_date=datetime.now() + timedelta(seconds=DEFAULT_RELEASE_SECONDS),
+            replace_existing=True,
+        )
